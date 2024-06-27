795f005b631323f3d043b5d46b31bd84b0eeba11
GoutamiGajakosh96 <goutamigajakosh96@gmail.com>
2024-06-27 17:05:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  region     = "ap-south-2"
-  AWS_ACCESS_KEY_ID: ${{ secrets.AWS_DEVOPS_ACCESS_KEY_ID }}
-  AWS_SECRET_ACCESS_KEY: ${{secrets.AWS_DEVOPS_SECRET_ACCESS_KEY }}
+  region= "us-east-1"
 }
