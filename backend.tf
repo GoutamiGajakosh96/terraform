c65d4c830388551016a4c7d66e938b0ae918dc5c
GoutamiGajakosh96 <goutamigajakosh96@gmail.com>
2024-06-27 17:13:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "github-actions-demo-9876"
-    key    = "github-actions-demo.tfstate"
+    bucket = "github-actions-demo-12-9876"
+    key    = "github-actions-demo-12.tfstate"
     region = "us-east-1"
   }
 }
